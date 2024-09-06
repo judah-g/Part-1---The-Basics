00e24a193af93833408405f35b66da4b68f31289
Judah Godden <339633265@GoTVDSB.ca>
2024-09-06 09:40:48 -0400

    yoy

--- Part 1 - The Basics/Program.cs
+++ Part 1 - The Basics/Program.cs
@@ -24,11 +24,32 @@ namespace Part_1___The_Basics
             Console.WriteLine(favMovie.Contains("THE"));
             Console.WriteLine();
 
-            favMovie = favMovie.Replace("A", "@");  favMovie = favMovie.Replace("E", "3");
+            favMovie = favMovie.Replace("A", "@"); favMovie = favMovie.Replace("E", "3");
             Console.WriteLine(favMovie);
             Console.WriteLine();
 
+            string quote = "Why buy a mattress anywhere else?";
+            quote = quote.Replace("a", ""); quote = quote.Replace("e", "");
+            quote = quote.Replace("i", ""); quote = quote.Replace("o", "");
+            quote = quote.Replace("u", ""); quote = quote.Replace("y", "");
+            Console.WriteLine(quote);
+            Console.WriteLine();
 
+            Console.Write("  _ 0                                             ");    Console.WriteLine("\t                ,-'\"\"\"\"`-._");
+            Console.Write("o'-/-\\--------------------------------------------");   Console.WriteLine("\t              ,' // <->    \"-.");
+            Console.Write("  |\\                                           . o");   Console.WriteLine("\t              | '` ,;;;....__|");
+            Console.Write("  / |       '.                             . o . o");    Console.WriteLine("\t              |   /._''''''''                       ,");
+            Console.Write("             .'                              o . o");    Console.WriteLine("\t               | `--._\"-._                         /|");
+            Console.Write("            '                                    o");    Console.WriteLine("\t                | ===|\"-__'                    _.-'/");
+            Console.Write("__________________________________________________");    Console.WriteLine("\t              .-: ==||-\"'\"-'--.          _..--' ,-'");
+            Console.WriteLine("\t\t\t\t\t\t\t  ,,--\"\"\"--._ ( |\\__||         |____----\"   _.-\"");
+            Console.WriteLine("\t\t\t\t\t\t\t  VV--\"\\ ,_  \"',\\  .-,`---.-,  `-_   __..--\"");
+            Console.WriteLine("\t\t\t\t\t\t\t,'_/_-\"`V' \"-./_ `\\.` `     |    _'\"\"");
+            Console.WriteLine("\t\t\t\t\t\t\t|__|__.-|     | \"-. `------' `,-\"");
+            Console.WriteLine("\t\t\t\t\t\t\t`.-\\-_ ,'      |  |\"\"\"---._,-\"'`]\"\"-.");
+            Console.WriteLine("\t\t\t\t\t\t\t  `-`-'        ;--;,        `._ /  ||");
+            Console.WriteLine("\t\t\t\t\t\t\t            ,\"\"`   |           ,\"  ||");
+            Console.WriteLine("\t\t\t\t\t\t\t            VV_.==='           VV--'");
         }
     }
 }
