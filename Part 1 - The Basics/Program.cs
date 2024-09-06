a410544b9b71c209271fa0b730f33524f79e998e
339633265 <339633265@2260I-L29-001>
2024-09-06 08:58:54 -0400

    Add project files.

---
+++ Part 1 - The Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // judah
+
+            string firstName = "Judah", favMovie = "The Matrix";
+
+            Console.WriteLine($"Hello, my name is {firstName} and my favourite movie is {favMovie.ToLower()}.");
+            Console.WriteLine();
+
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine();
+
+            Console.WriteLine(favMovie.Contains("THE"));
+            Console.WriteLine();
+
+            favMovie = favMovie.Replace("A", "@");  favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+            Console.WriteLine();
+
+
+        }
+    }
+}
